9af6c8c1d7565e54b67cf8f820f9f0ee8ccb6455
danielcueto <daniel.cueto@assuresoft.tech>
2025-05-28 15:20:32 -0400

    little change

--- src/components/modals/ModalConfirmDelete.tsx
+++ src/components/modals/ModalConfirmDelete.tsx
@@ -22,7 +22,7 @@ export function ModalConfirmDelete({ isOpen, onClose, onConfirm, itemName }: Mod
         </button>
         <button
           onClick={onConfirm}
-          className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700"
+          className="px-4 py-2 bg-red-400 text-white rounded hover:bg-red-600"
         >
           Delete
         </button>
